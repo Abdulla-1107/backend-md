332c22ab25984e1197eb4b98f67d6baa001dcde8
Abdulla <abdusattorovabdulla60@gmail.com>
2026-01-27 11:39:39 +0500

    add

---
+++ src/product/dto/create-product.dto.ts
@@ -0,0 +1,71 @@
+import {
+  IsString,
+  IsNotEmpty,
+  IsDecimal,
+  IsOptional,
+  IsUrl,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Decimal } from '@prisma/client/runtime/index-browser';
+
+export class CreateProductDto {
+  @ApiProperty({
+    example: 'Mahsulot nomi',
+    description: 'Product name in Uzbek',
+  })
+  @IsString()
+  @IsNotEmpty()
+  name_uz: string;
+
+  @ApiProperty({
+    example: 'Product name',
+    description: 'Product name in English',
+  })
+  @IsString()
+  @IsNotEmpty()
+  name_en: string;
+
+  @ApiProperty({
+    example: 'Название продукта',
+    description: 'Product name in Russian',
+  })
+  @IsString()
+  @IsNotEmpty()
+  name_ru: string;
+
+  @ApiProperty({
+    example: 'Mahsulot tavsifi',
+    description: 'Product description in Uzbek',
+  })
+  @IsString()
+  @IsNotEmpty()
+  description_uz: string;
+
+  @ApiProperty({
+    example: 'Описание продукта',
+    description: 'Product description in Russian',
+  })
+  @IsString()
+  @IsNotEmpty()
+  description_ru: string;
+
+  @ApiProperty({
+    example: 'Product description',
+    description: 'Product description in English',
+  })
+  @IsString()
+  @IsNotEmpty()
+  description_en: string;
+
+  @ApiProperty({ example: 99.99, description: 'Product price' })
+  @IsNotEmpty()
+  price: number | Decimal;
+
+  @ApiProperty({
+    example: 'https://example.com/image.jpg',
+    description: 'Product image URL',
+  })
+  @IsString()
+  @IsNotEmpty()
+  image: string;
+}
